88bace0acef84e8c5a0e74581a5dc25e5c41fa4b
William Muya <mwilliamm91@users.noreply.github.com>
2022-11-15 01:42:53 +0000

    completed week 2 project as per client's specifications

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import csv
+import os
 # ***********************variables***********************
 products = ["coke","fanta","water"]
-customer_orders = []
+all_orders = []
 order_status = ["Preparing", "Awaiting Pickup", "Out for Delivery", "Delivered"]
 
 # ******** functions**************
 
-# imports customer orders and appends the orders to customer_orders
+# imports customer orders and appends the orders to all_orders
 
 def import_orders():
     try:
         with open("./orders.csv", "r") as file:
             contents = csv.DictReader(file)
             for row in contents:
-                customer_orders.append(row)
+                all_orders.append(row)
     except FileNotFoundError as fnfe:
         print(f"Error: {fnfe}")
 
@@ -22,9 +23,9 @@ import_orders()
 # ***************main app function*****************
 def main_menu_display():
     print('''
-          0. Quit the app
-          1. Show product menu
-          2. Show order list \n 
+        0. Quit the app
+        1. Show product menu
+        2. Show order list \n 
           ''')
 # ****************Product menu display*******************
 def display_product_menu():
@@ -39,12 +40,12 @@ def display_product_menu():
 #  ***************Order menu display**********************
 def display_order_menu():
     print('''-----ORDERS-----
-        0. Exit
-        1. View Order List
-        2. Add an order
-        3. Update an order's status
-        4. Update an order
-        5. Remove an order
+    0. Exit
+    1. View Order List
+    2. Add an order
+    3. Update an order's status
+    4. Update an order
+    5. Remove an order
 ---------------------''')
 
 # adds new orders into a list
@@ -56,53 +57,88 @@ def customer_details_input():
     customer_address = input("Enter the customer's address")
     customer_phone_number = input("Enter the customer's phone number")
       
-    new_customer_orders.append({"customer_name": customer_name, "customer_address":customer_address, "customer_phone_number": customer_phone_number, "status": "pending"})
+    new_customer_orders.append({"Customer_name": customer_name, "Customer_address":customer_address, "Customer_phone_number": customer_phone_number, "Status": "pending"})
  
 # writes the orders in a separate file (orders.csv)    
 def writes_orders(new_orders):
     try:
-        with open("./orders.csv", "w") as file:
-            fieldnames = ["customer_name", "customer_address", "customer_phone_number", "status"]
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for order in new_orders:
-                writer.writerow(order)
+        orders_file = "./orders.csv"
+        if os.path.isfile(orders_file):
+            with open("./orders.csv", "a", newline="") as file:
+                fieldnames = ["Customer_name", "Customer_address", "Customer_phone_number", "Status"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                for new_order in new_orders:
+                    writer.writerow(new_order)
+                    new_orders.remove(new_order)
+        else:
+            with open(orders_file, "w", newline="") as file:
+                fieldnames = ["Customer_name", "Customer_address", "Customer_phone_number", "Status"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                for new_order in new_orders:
+                    writer.writerow(new_order)
+                    new_orders.remove(new_order)      
     except IOError:
         print("I/O error")
+
         
 # displays indexed customer orders
 def indexed_orders():
-    for i in range(len(customer_orders)):
-        print(customer_orders.index(i), ":", i)
+    for ind, val in enumerate(all_orders):
+        print(ind, ":", val )
+       
 
 def changes_order_status(order_index, status_index):
-    if order_index in range(len(indexed_orders)):
+    if order_index in range(len(all_orders)):
         if status_index in range(len(order_status)):
-            current_order_status = customer_orders[order_index]['status']
-            customer_orders[order_index]["status"]= order_status[status_index]
+            current_order_status = all_orders[order_index]['Status']
+            all_orders[order_index]["status"]= order_status[status_index]
             print(f"Order status has been changed from {current_order_status} to {order_status[status_index]}\n")
-            print(customer_orders[order_index])
+            print(all_orders[order_index])
+    else:
+        print("Incorrect input or out of range")
 
 def changes_key_value_properties(order_index):
-    if order_index in range(len(customer_orders)) and order_index != '':
-         
-        user_input_data = customer_orders[order_index]
-        
-        
-        for key, value in user_input_data.items():
-            print(key, value)
-        print(customer_orders)
+    if order_index not in range(len(all_orders)) and order_index == '':
+         print("No property has been changed")
     else:
-        print("No property has been changed")
-            
+        print("\n", all_orders[order_index], "\n")
+        for key in all_orders[order_index].keys():
+            while True:
+                user_input= input(f"Would you like to change {key}? Enter Y for yes or N for no. \n").title()
+                if user_input != "N" and user_input != "Y":
+                    print("Invalid input")
+                    break
+                elif user_input == "N":
+                    print(f"{key} wasn't changed")
+                    break
+                else:
+                    value_input = input(f"Enter the new {key} \n")
+                    all_orders[order_index].update({key:value_input})
+                    print(f"{key}:{value_input}")
+                    break
+        orders_file = "./orders.csv"
+        with open(orders_file, "w", newline="") as file:
+            fieldnames = ["Customer_name", "Customer_address", "Customer_phone_number", "Status"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(all_orders)
 
+# **************deletes an order from an indexed list given the index and updates the txt file*********************
 
-
+def deletes_order(order_index):
+    if order_index in range(len(all_orders)):
+        all_orders.remove(all_orders[order_index])
+    orders_file = "./orders.csv"
+    with open(orders_file, "w", newline="") as file:
+        fieldnames = ["Customer_name", "Customer_address", "Customer_phone_number", "Status"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(all_orders)
 # *********************welcome message*******************
 print("Welcome to the app \n")
 
 while True:
-    
     main_menu_display()
     #  GET user input for main menu option
     start_app_input = int(input("Select input as shown above \n"))
@@ -165,11 +201,11 @@ while True:
                 print("....returning to main menu \n")
                 break
             elif order_menu_options == 1:
-                print(f"There are {len(customer_orders)} orders")
-                if len(customer_orders) == 0:
+                print(f"There are {len(all_orders)} orders")
+                if len(all_orders) == 0:
                     pass
                 else:
-                    print(f"Orders are shown below: \n{customer_orders}")
+                    print(f"Orders are shown below: \n{all_orders}")
                 continue
             elif order_menu_options == 2:
                 customer_details_input()
@@ -177,17 +213,22 @@ while True:
                 continue
             elif order_menu_options == 3:
                 indexed_orders()
-                order_index_input = int(input("Enter the order index to proceed\n"))
-                for i in range(len(order_status)):
-                    print(order_status.index(i), ":", i)
-                order_status_input = int(input("Enter the chosen order status index as displayed above\n"))
+                order_index_input = int(input("Enter the order index to proceed \n"))
+                
+                for (index, value) in enumerate(order_status):
+                    print(index, ":", value)
+                order_status_input = int(input("Enter the chosen order status index as displayed above \n"))
                 changes_order_status(order_index_input,order_status_input)               
                 continue
             elif order_menu_options == 4:
                 indexed_orders()
-                selected_input = int(input("Enter the chosen order status index as displayed above\n"))
+                selected_input = int(input("Enter the index to update \n"))
                 changes_key_value_properties(selected_input)
                 continue
+            elif order_menu_options == 5:
+                indexed_orders()
+                selected_input = int(input("Enter the index to delete\n"))
+                deletes_order(selected_input)  
             else:
                 print("Enter a valid input")
             
