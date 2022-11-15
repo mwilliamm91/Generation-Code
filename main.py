f36961496c15c761218d2648c772ad8d8a7a82e4
William Muya <mwilliamm91@users.noreply.github.com>
2022-11-15 16:13:00 +0000

    Week three specifications complete, added products and courier files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv
 import os
 # ***********************variables***********************
-products = ["coke","fanta","water"]
+products = []
 all_orders = []
+all_couriers = []
 order_status = ["Preparing", "Awaiting Pickup", "Out for Delivery", "Delivered"]
 
 # ******** functions**************
@@ -21,35 +22,47 @@ def import_orders():
 import_orders()
 
 # ***************main app function*****************
-def main_menu_display():
+def displays_main_menu():
     print('''
         0. Quit the app
         1. Show product menu
-        2. Show order list \n 
+        2. Show the courier menu
+        3. Show order menu \n 
           ''')
 # ****************Product menu display*******************
-def display_product_menu():
+def displays_product_menu():
     print("""-----PRODUCTS-----
-    0. Exit 
+    0. Return to main menu
     1. View Product List
-    2. Add a Product
+    2. Add a new Product
     3. Update Product
     4. Delete Product
 ---------------------""")
 
 #  ***************Order menu display**********************
-def display_order_menu():
+def displays_order_menu():
     print('''-----ORDERS-----
-    0. Exit
+    0. Return to main menu
     1. View Order List
-    2. Add an order
+    2. Add a new order
     3. Update an order's status
     4. Update an order
     5. Remove an order
 ---------------------''')
 
-# adds new orders into a list
+# **************Courier menu display***********************
 
+def displays_courier_menu():
+    print('''------Couriers-------
+          0: Return to main menu
+          1: View courier list
+          2: Add a new courier
+          3: Update an existing courier
+          4: Delete a courier from the list
+-----------------------''')
+
+
+# adds new orders into a list
 new_customer_orders = []
 
 def customer_details_input():
@@ -87,7 +100,7 @@ def indexed_orders():
     for ind, val in enumerate(all_orders):
         print(ind, ":", val )
        
-
+# ********** changes the order status of an order when passed the order's index*********************
 def changes_order_status(order_index, status_index):
     if order_index in range(len(all_orders)):
         if status_index in range(len(order_status)):
@@ -97,8 +110,8 @@ def changes_order_status(order_index, status_index):
             print(all_orders[order_index])
     else:
         print("Incorrect input or out of range")
-
-def changes_key_value_properties(order_index):
+# ********** loops through the key, value pair and changes the value if user requests*****************
+def changes_key_value(order_index):
     if order_index not in range(len(all_orders)) and order_index == '':
          print("No property has been changed")
     else:
@@ -124,7 +137,7 @@ def changes_key_value_properties(order_index):
             writer.writeheader()
             writer.writerows(all_orders)
 
-# **************deletes an order from an indexed list given the index and updates the txt file*********************
+# *******deletes an order from an indexed list given the index and updates the txt file*********************
 
 def deletes_order(order_index):
     if order_index in range(len(all_orders)):
@@ -135,11 +148,53 @@ def deletes_order(order_index):
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(all_orders)
+
+# *********displays all orders***************
+def displays_all_couriers():
+    with open("./couriers.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            all_couriers.append(line)
+# loads the couriers list
+displays_all_couriers()
+        
+# Adds a new courier and saves to a list and text file
+new_couriers = []
+def adds_courier(couriers):
+    new_courier = input("Enter the new couriers' name.\n")
+    couriers.append(new_courier)
+
+# **************writes to couriers.txt*************
+    try:
+        with open("./couriers.txt", "a") as file: 
+            for courier in couriers:
+                file.write(courier + '\n')
+                couriers.remove(courier)
+    except FileNotFoundError as fnf:
+        print(f"{fnf}:File failed to open")
+    except Exception as e:
+        print(e)
+    
+# *************** Updates couriers.txt*******************
+
+def updates_couriers_file(courier_list):
+    # Updates name in the text file
+                try:
+                    with open("./couriers.txt", "w") as file:
+                        for courier in courier_list:
+                            file.writelines(courier + "\n")
+                except:
+                    print("File failed to open")
+                    
 # *********************welcome message*******************
-print("Welcome to the app \n")
+
+print("\n Welcome to the app \n")
+
+# ******************Main app section**********************
 
 while True:
-    main_menu_display()
+    displays_main_menu()
     #  GET user input for main menu option
     start_app_input = int(input("Select input as shown above \n"))
     # check for valid input
@@ -153,7 +208,7 @@ while True:
     elif start_app_input == 1:
     # input selections for the product menu    
         while True: 
-            display_product_menu()  
+            displays_product_menu()  
         
             product_menu_options = int(input("Select input as shown above \n"))
             
@@ -191,9 +246,47 @@ while True:
                 continue
         
     elif start_app_input == 2:
-
         while True:
-            display_order_menu()
+            displays_courier_menu()
+            
+            courier_menu_options = int(input("Select an input as shown above \n"))
+            if courier_menu_options == 0:
+                print("....returning to main menu \n")
+                break
+            elif courier_menu_options == 1:
+                print(f"There are {len(all_couriers)} couriers as shown below:")
+                print(f"\n{all_couriers}\n")
+                continue
+            elif courier_menu_options == 2:
+                adds_courier(new_couriers)
+                continue
+            elif courier_menu_options == 3:
+                for index, value in enumerate(all_couriers):
+                    print(index, ":", value)
+                courier_index_input = int(input("\n Enter the index to update an existing courier \n"))
+                print(f"\nYou have selected {all_couriers[courier_index_input]}\n")
+                courier_name_update = input("Enter courier's new name\n")
+                all_couriers[courier_index_input] = courier_name_update
+                
+                # update the courier file
+                updates_couriers_file(all_couriers)
+                continue
+            elif courier_menu_options == 4:
+                for index, value in enumerate(all_couriers):
+                    print(index, ":", value)
+                courier_index_input = int(input("\n Enter the index to delete an existing courier \n"))
+                print(f"\nYou have deleted {all_couriers[courier_index_input]}\n")
+                del all_couriers[courier_index_input]
+               
+                # update the courier file
+                updates_couriers_file(all_couriers)
+                continue
+            else:
+                print("Enter a valid input!")
+                
+    elif start_app_input == 3:
+         while True:
+            displays_order_menu()
             
             order_menu_options = int(input("Select input as shown above \n"))
             
@@ -201,11 +294,11 @@ while True:
                 print("....returning to main menu \n")
                 break
             elif order_menu_options == 1:
-                print(f"There are {len(all_orders)} orders")
+                print(f"There are {len(all_orders)} orders as shown below:")
                 if len(all_orders) == 0:
                     pass
                 else:
-                    print(f"Orders are shown below: \n{all_orders}")
+                    print(f"\n{all_orders}\n")
                 continue
             elif order_menu_options == 2:
                 customer_details_input()
@@ -223,7 +316,7 @@ while True:
             elif order_menu_options == 4:
                 indexed_orders()
                 selected_input = int(input("Enter the index to update \n"))
-                changes_key_value_properties(selected_input)
+                changes_key_value(selected_input)
                 continue
             elif order_menu_options == 5:
                 indexed_orders()
